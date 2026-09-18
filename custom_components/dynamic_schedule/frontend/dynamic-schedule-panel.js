1d33a48dce2b5a519599c8915dc7e9cff2e81f1f
Alastair <al.kirngeo@gmail.com>
2026-09-18 20:48:53 +0100

    a

--- custom_components/dynamic_schedule/frontend/dynamic-schedule-panel.js
+++ custom_components/dynamic_schedule/frontend/dynamic-schedule-panel.js
@@ -415,9 +415,9 @@ class DynamicSchedulePanel extends HTMLElement {
           box-sizing: border-box;
         }
 
-*.div {
+.div {
    outline:1px blue solid;
-   padding: 1px;
+   padding: 2px;
  }
 
         * {
