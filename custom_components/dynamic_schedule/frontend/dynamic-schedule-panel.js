82eec5b0132ad09cfd03c8fce5c0a7197dab2829
Alastair <al.kirngeo@gmail.com>
2026-09-19 10:44:09 +0100

    a

--- custom_components/dynamic_schedule/frontend/dynamic-schedule-panel.js
+++ custom_components/dynamic_schedule/frontend/dynamic-schedule-panel.js
@@ -262,6 +262,7 @@ class DynamicSchedulePanel extends HTMLElement {
 
   async fetchScriptDetails() {
       console.log( '_hass keys', Object.keys( this._hass ) );
+      console.log( 'config', this._hass.config );
       console.log( 'config keys', Object.keys( this._hass.config ) );
       console.log( 'config.components', this._hass.config.components );
       console.log( 'config.config_source', this._hass.config.config_source );
