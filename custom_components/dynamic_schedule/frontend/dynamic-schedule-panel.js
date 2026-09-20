474d6d5da0e3587797b52f5222d429d62ca574fe
Alastair <al.kirngeo@gmail.com>
2026-09-20 19:10:58 +0100

    a

--- custom_components/dynamic_schedule/frontend/dynamic-schedule-panel.js
+++ custom_components/dynamic_schedule/frontend/dynamic-schedule-panel.js
@@ -375,8 +375,9 @@ class DynamicSchedulePanel extends HTMLElement {
                           let filxt = ( Array.isArray(sequence) ? sequence : [sequence])
                               .map( (seq)=>{ console.log('seq', seq)} ) ;
                           let filt = ( Array.isArray(sequence) ? sequence : [sequence])
-                              .filter( (seq)=>{ seq.response_variable} ) ;
+                              .filter( (seq)=>{ seq.response_variable !== null} ) ;
                           console.log('filt', filt);
+                          console.log('filtx', filt);
                           return {
                               alias: stateObj.attributes.friendly_name || config.alias || entityId.split('.')[1],
                               response_variable: true,
