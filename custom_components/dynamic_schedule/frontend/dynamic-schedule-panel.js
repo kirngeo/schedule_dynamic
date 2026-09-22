bab9e962d032a0672b91912c7c806054a921a80a
Alastair <al.kirngeo@gmail.com>
2026-09-22 22:32:00 +0100

    a

--- custom_components/dynamic_schedule/frontend/dynamic-schedule-panel.js
+++ custom_components/dynamic_schedule/frontend/dynamic-schedule-panel.js
@@ -113,12 +113,14 @@ class DynamicSchedulePanel extends HTMLElement {
           let ent = {at: {hh: 1, mm:2, ss:3}, state: 45};
           let ent2 = {at: {hh: 9, mm:0}, state: 18};
           let ent3 = {at: {hh: 12}, state: 31};
+          let ent4 = {at: {hh: 23, mm: 59, ss: 21}, state: 22}; 
    //       dummy.sub_schedules['Tue'].transitions.push( ent);
    //       dummy.sub_schedules['Tue'].transitions.push( ent2);
    //       dummy.sub_schedules['Tue'].transitions.push( ent3);
+          dummy.sub_schedules['Tue'].transitions.push( ent4);
     //      dummy.name = 'Lounge Heating';
-          delete dummy.sub_schedules['Off'];
-          delete dummy.sub_schedules['Mon'];
+    //      delete dummy.sub_schedules['Off'];
+    //      delete dummy.sub_schedules['Mon'];
 
           delete dummy.attributes['next_event'];
           delete dummy.attributes['next_state'];
